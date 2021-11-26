3f31d9b2710ac343e42c21a0e7847a06dc8f53d1
WhoAmI-T1T4N <92716161+WhoAmI-T1T4N@users.noreply.github.com>
2021-11-26 12:43:21 +0700

    Create Tools-4SIA.sh

---
+++ Tools-4SIA.sh
@@ -0,0 +1,296 @@
+# mau ngapain bang?
+# [----AUTHOR BY T1T4N----]
+# Coppyright by Mugiwara And Titan
+# Thanks To All
+# Member Of, MBT | ITL | LCT
+# feat 0xMH4CK
+
+green='\033[0;32m'
+greenlight='\033[32;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+black='\033[30;1m'
+yellow='\033[33;1m'
+yellowlight='\033[1;33m'
+blue='\033[36;1m'
+bluelight='\033[0;36m'
+
+#BUAT REGISTRASI DULU
+clear
+echo $blue "=============================="
+echo $blue "======"$black "https://bit.ly"$blue "========"
+echo $blue "=============================="
+echo $red "UTAMAKAN UNTUK SUBSCRIBE"
+echo $red "Prioritize Subscribe"
+echo $yellow "T1T4N TZY"
+    read -p "User =>" user
+    read -p "Pass =>" pass
+#Usernya
+if [ $user = "Mugiwara" ]
+       then
+       echo $yellowlight "#############"
+       echo $yellowlight "# SUCCCESSS #"
+       echo $yellowlight "#############"
+       sleep 0.1
+       else
+       echo $red "##########"
+       echo $red "# FAILED #"
+       echo $red "##########"
+       sleep 3
+       sh IndonesiaLover.sh
+       sleep 0.1
+fi
+#passwordnya
+if [ $pass = "ITL" ]
+       then
+       echo $yellowlight "############"
+       echo $yellowlight "# SUCCCESS #"
+       echo $yellowlight "############"
+       sleep 0.1
+       else
+       echo $red "##########"
+       echo $red "# FAILED #"
+       echo $red "##########"
+       sleep 3
+       sh IndonesiaLover.sh
+       sleep 0.1
+fi
+#tampilan
+# substitle
+clear
+      echo $blue "What Language Do You USE IT"                                                                                   
+      echo $blue "SELECT 2, IF YOU LANGUAGE"$red "ENGLISH"                                                                        
+      echo $white "SELETCT 1, JIKA KAMU BAHASA"$red "INDONESIA"
+      echo "========================"                                                                                             
+      echo "1]. Indonesia"                                                                                                        
+      echo "2]. English"                                                                                                         
+      echo "Bahasa Or Language"                                                                                                   
+      read -p "=>" bus                                                                                                       
+      if [ $bus = 1 ]
+then
+if [ $bus = 1 ] || [ $bus = 1 ]
+then
+clear
+echo $red "YOU USE, ENGLISH LANGUAGE"                                                                                       
+echo $red "Sorry, It's Still In"
+echo $red "DEVELOPMENT"                                                                                                     
+echo
+fi
+if [ $bus = 2 ] || [ $bus = 2 ]
+then
+clear
+echo $red "KAMU AKAN DI ARAHKAN"                                                                                            
+echo $red "KE TOOLSNYA"                                                                                                    
+echo
+clear
+echo $yellow "WAITT, SEDANG RELOAAD"                                                                                        
+sleep 0.1                                                                                                                   
+clear                                                                                                                       
+echo $yellow "====--------" "10%"                                                                                           
+sleep 0.1                                                                                                                   
+clear                                                                                                                       
+echo $yellow "=======-----" "50%"                                                                                           
+sleep 0.1                                                                                                                   
+clear                                                                                                                       
+echo $yellow "==========--" "81%"                                                                                           
+sleep 0.1
+clear
+echo $yellow "============" "100%"                                                                                          
+sleep 0.1                                                                                                                   
+clear
+echo "SUCCES, TINGGAL INSTALL BAHANNYA DEHH"
+pkg install git
+echo "GIT TELAH DI INSTALL"
+pkg install python
+echo "PYTHON TELAAH DI INSTALL"
+pip2 install requests
+echo "REQUESTS TELAH DI MASUKAN"
+apt insttall -y dnsuntil
+echo "DSUNTILL TELAH DI INSTALL"                                                                                                                                                                                                          
+clear
+echo "[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+echo "[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "[>>>                          <<<]"
+sleep 0.1
+clear
+echo "[>>                            <<]"
+sleep 0.1
+clear
+echo "[>                              <]"
+sleep 0.1
+clear
+echo "[>                              <]"
+sleep 0.1
+clear
+echo "[>>                            <<]"
+sleep 0.1
+clear
+echo "[>>>                          <<<]"
+sleep 0.1
+clear
+echo "[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+sleep 1
+echo $black "=============================================="
+echo $blue  "Author : Mr.T1T4N"
+echo $blue  "Feat : 0xMH4CK"
+echo $blue  "Team : Indonesia Teach Learning | Mugiwara BlackHat | Legend Cyber"
+echo $blue  "Mastah : Jangan Pernah TEBAS SHELL Orang, Girilan Shell Sendiri Di Tebas Nangiss"
+echo $black "============================================="
+echo $red   "1]. Tools Spesial ID"
+echo $red   "2]. DarkFly"
+echo $red   "3]. Tools Mugiwara ID"
+echo $red   "4]. Kasih Virus Ke ORANG"
+echo $red   "5]. DDOS LOW"
+echo $red   "6]. DDOS MUGIWARA"
+echo $red   "7]. DDOS LOW BUT 2"
+echo $red   "8]. DDOS HIGH"
+echo $red   "9]. DDOS HIGHT BUT 2"
+echo $red   "10]. DDOS BPC"
+echo $red   "11]. DDOS PentMenu"
+echo $red   "12]. DDOS Random"
+echo $red   "13]. Install Socks"
+echo $red   "14]. Sadap Pacar"
+echo $red   "15]. Encrypt Python Or Bash"
+echo $red   "16]. Spam Mantan"
+echo $red   "17]. Virtex"
+echo $red   "18]. Spam DOI"
+echo $red   "19]. Spam SMS Mantan"
+echo $red   "20]. Tampilan Keren Termux"
+echo $red   "21]. Pulsa Gratis Seumur Hidup"
+echo $red   "00]. EXIT FROM Tools"
+echo $black "============================================="
+   echo $green "Pilih Aja Toolsnya"
+   read -p "=>" gila
+fi
+if [ $gila = 1 ] || [ $gila = 1 ]
+then
+    git clone https://github.com/WhoAmI-T1T4N/ToolS-ID2
+       cd ToolS-ID2
+         sh Tools-4J4X.sh
+fi
+if [ $gila = 2 ] || [ $gila = 2 ]
+then
+    git clone https://github.com/Ranginang67/DarkFly-Tool
+      cd DarkFly-Tool
+        python2 install.py
+          DarkFly
+fi
+if [ $gila = 3 ] || [ $gila = 3 ]
+then
+    git clone https://github.com/WhoAmI-T1T4N/MyTooLS-IPec
+      cd MyTooLS-IPec
+        sh MugiWARTools.sh
+fi
+if [ $gila = 4 ] || [ $gila = 4 ]
+then
+    git clone https://github.com/Nymouse-X/SendVirus-Trojan
+       cd SendVirus-Trojan
+          bash CyberSploit.sh
+fi
+if [ $gila = 5 ] || [ $gila = 5 ]
+then
+   git clone https://github.com/BLACK-PHANTOM-CYBER/Dos-stealh
+      cd Dos-stealh
+        python2 mec.py
+fi
+if [ $gila = 6 ] || [ $gila = 6 ]
+then
+  git clone https://github.com/WhoAmI-T1T4N/DDoS-EPIC-ID
+     cd DDoS-EPIC-ID
+       python2 DDoS-EPIC-ID
+fi
+if [ $gila = 7 ] || [ $gila = 7 ]
+then
+  git clone htps://github.com/BLACK-PHANTOM-CYBER/Dos-stealh
+   cd Dos-stealh
+      python2 dosmec.py
+fi
+if [ $gila = 8 ] || [ $gila = 8 ]
+then
+   git clone https://github.com/BLACK-PHANTOM-CYBER/DDOS2
+     cd DDOS2
+       python2 high.py
+fi
+if [ $gila = 9 ] || [ $gila = 9 ]
+then
+  git clone https://github.com/BLACK-PHANTOM-CYBER/DDOS2
+     cd DDOS2
+        python2 low.py
+fi
+if [ $gila = 10 ] || [ $gila = 10 ]
+then
+  git clone https://github.com/BLACK-PHANTOM-CYBER/DDOS-BPC
+      cd DDOS-BPC
+         python2 SPOONDDOS.py
+fi
+if [ $gila = 11 ] || [ $gila = 11 ]
+then
+  git clone https://github.com/GinjaChris/pentmenu
+      cd pentmenu
+         ./pentmenu
+fi
